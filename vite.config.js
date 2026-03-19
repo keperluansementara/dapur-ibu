5b61ecf03723b8b285f157441d5eaa5aa07961b0
keperluansementara <keperluansementara@gmail.com>
2026-03-20 00:35:29 +0700

    feat: Configure GitHub Pages deployment, add related scripts and dependency, and update project name.

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,8 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  // Ganti 'katering-landing-page' dengan nama repository GitHub Anda yang sebenarnya
+  base: '/dapur-ibu/',
   plugins: [
     react(),
     tailwindcss(),
